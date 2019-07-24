0d8aa481ce032bc63f85084742f62045847e5226
saziknows <52761798+saziknows@users.noreply.github.com>
2019-07-24 20:33:41 +0200

    Add files via upload

---
+++ django-env.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo '
+ ______  _________ _______  _        _______  _______ 
+(  __  \ \__    _/(  ___  )( (    /|(  ____ \(  ___  )
+| (  \  )   )  (  | (   ) ||  \  ( || (    \/| (   ) |
+| |   ) |   |  |  | (___) ||   \ | || |      | |   | |
+| |   | |   |  |  |  ___  || (\ \) || | ____ | |   | |
+| |   ) |   |  |  | (   ) || | \   || | \_  )| |   | |
+| (__/  )|\_)  )  | )   ( || )  \  || (___) || (___) |
+(______/ (____/   |/     \||/    )_)(_______)(_______)
+                                                      
+
+'
+
+#brew install python3
+#brew install pip3
+
+mkdir 'Django'
+
+cd Django
+
+pip3 install virtualenv
+
+virtualenv env
+
+source env/bin/activate
+
+pip3 install django
+
+echo "Name your Django app: "
+
+read varname 
+
+django-admin startproject $varname
+
